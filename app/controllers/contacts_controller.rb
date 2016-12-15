44f57170bad1f68e80ca37fc312a026fadff48af
Veronica Maxwell <maxwellveronica1@gmail.com>
2016-12-15 13:32:02 +0000

    Add create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
     def new
+        @contact = Contact.new
     end
+    
+    def create
+       @contact = Contact.new(contact_params)
+        if @contact.save
+           redirect_to new_contact_path, notice: "Thank you. Message sent."
+        else
+            redirect_to new_contact_path, notice: "There was a problem with your form."
+        end
+    end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end    
 end
